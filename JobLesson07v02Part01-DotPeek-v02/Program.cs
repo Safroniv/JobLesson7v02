cd0c5eca98b47478815c8f886d0d785237d77cda
Safroniv <safroniv@mail.ru>
2022-02-21 19:39:59 +0300

    Урок 7 Задание 1 - Пересохранение через dotPeek, изменение условия if с 10 на 12 и пересборка проекта

---
+++ JobLesson07v02Part01-DotPeek-v02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: JobLesson07v02Part01.Program
+// Assembly: JobLesson07v02Part01, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 2E27D3B8-260F-4EEC-B792-D9ED6FFEDC2D
+// Assembly location: C:\Users\Safjon\YandexDisk\++Programming\++LernCShrp++\Proj_GeekBrains\01-1qua-3EntrCShrp\Lesn07\JobLesson07v02\JobLesson07v02Part01\bin\Debug\JobLesson07v02Part01.exe
+
+using System;
+
+namespace JobLesson07v02Part01
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Input b");
+      if (int.Parse(Console.ReadLine()) == 12)
+        Console.WriteLine("Hallo World");
+      else
+        Console.WriteLine("b not a");
+      Console.ReadLine();
+    }
+  }
+}

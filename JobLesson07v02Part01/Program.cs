17058c89b265991ec79e82aed76b89e6c7eeb8e9
Safroniv <safroniv@mail.ru>
2022-02-21 19:27:39 +0300

    Добавьте файлы проекта.

---
+++ JobLesson07v02Part01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobLesson07v02Part01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input b");
+            int b = Int32.Parse(Console.ReadLine());
+            int a = 10;
+
+            if (b == a)
+            {
+                Console.WriteLine("Hallo World");
+            }
+            else
+            {
+                Console.WriteLine("b not a");
+            }
+            Console.ReadLine();
+        }
+    }
+}
